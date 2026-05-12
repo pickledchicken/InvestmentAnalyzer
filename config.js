e92b97f66b8bfeff9f47c584dff51eef005de115
pickledchicken <41799108+pickledchicken@users.noreply.github.com>
2026-05-12 16:25:41 -0400

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-/*
-  After you deploy your Render backend, replace this value.
-
-  Example:
-  const API_BASE_URL = "https://your-render-service-name.onrender.com";
-*/
-
-const API_BASE_URL = "https://REPLACE-WITH-YOUR-RENDER-URL.onrender.com";
